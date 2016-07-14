8ffc3a02a71bbfd9411e4379120c5161fb79623c
aclassens <aclassens@REA5116.reacollege.eu>
2016-07-14 14:49:39 +0200

    links foto's aangepast

--- view/voorstellen.php
+++ view/voorstellen.php
@@ -6,33 +6,33 @@ function showVoorstellen()
         <h2> Even voorstellen </h2>
             <h3>De hele commissie </h3>
             	<figure><div class="commissie">
-                    <img src="../images/voorstellen/hele_commissie.jpg" width="244" height="244" alt="alle commissie leden" />
+                    <img src="images/voorstellen/hele_commissie.jpg" width="244" height="244" alt="alle commissie leden" />
                     <figcaption>Van links naar rechts: Ronald den Otter, Anouk Classens, John van Es, Monique Aspers-Counotte  en Leo Janssen</figcaption></div>
                 </figure>
             
             <h3> Inloopavond commissie </h3>
                 <figure>
                 <div class="commissie">
-                    <img src="../images/voorstellen/inloop_commissie.jpg" alt="" width="193" height="193" title="Anouk Classens"/>
+                    <img src="images/voorstellen/inloop_commissie.jpg" alt="" width="193" height="193" title="Anouk Classens"/>
                   <figcaption>Inloopavond commissie</figcaption></div>
                 <div class="commissie">
-                    <img src="../images/voorstellen/anouk.jpg" alt="" width="128" height="193" title="Anouk Classens"/>
+                    <img src="images/voorstellen/anouk.jpg" alt="" width="128" height="193" title="Anouk Classens"/>
                     <figcaption>Anouk Classens</figcaption></div>
                 <div class="commissie">
-                    <img src="../images/voorstellen/leo.JPG" alt="" width="128" height="193" title="Leo Janssen"/>
+                    <img src="images/voorstellen/leo.JPG" alt="" width="128" height="193" title="Leo Janssen"/>
                     <figcaption>Leo Janssen</figcaption></div>
                 </figure>
 
             <h3> Activiteiten commissie </h3>
                 <figure>
                 <div class="commissie">
-                    <img src="../images/voorstellen/activiteiten commisie.jpg" alt="" width="193" height="193" title="Activiteiten commissie"/>
+                    <img src="images/voorstellen/activiteiten commisie.jpg" alt="" width="193" height="193" title="Activiteiten commissie"/>
                   <figcaption>Activiteiten commissie</figcaption></div>
                 <div class="commissie">
-                    <img src="../images/voorstellen/leo.JPG" alt="" width="128" height="193" title="Leo Janssen"/>
+                    <img src="images/voorstellen/leo.JPG" alt="" width="128" height="193" title="Leo Janssen"/>
                     <figcaption>Leo Janssen</figcaption></div>
                 <div class="commissie">
-                    <img src="../images/voorstellen/ronald.jpg" alt="" width="128" height="193" title="Ronald den Otter"/>
+                    <img src="images/voorstellen/ronald.jpg" alt="" width="128" height="193" title="Ronald den Otter"/>
                     <figcaption>Ronald den Otter</figcaption>
                     </div>
                 </figure>
@@ -40,19 +40,19 @@ function showVoorstellen()
             <h3> Communicatie commissie </h3>
                 <figure>
             	<div class="commissie" >
-            		<img src="../images/voorstellen/communicaite_commissie.jpg" alt="" width="193" height="193" title="Monique Aspers-Counotte"/>
+            		<img src="images/voorstellen/communicaite_commissie.jpg" alt="" width="193" height="193" title="Monique Aspers-Counotte"/>
             		<figcaption>Communicatie commissie</figcaption>
               	</div> 
                 <div class="commissie" >
-                	<img class="commissie" src="../images/voorstellen/anouk.jpg" alt="" width="128" height="193" title="Anouk Classens"/> 
+                	<img class="commissie" src="images/voorstellen/anouk.jpg" alt="" width="128" height="193" title="Anouk Classens"/> 
                 	<figcaption>Anouk Classens</figcaption>
                 </div>    
                 <div class="commissie">
-                	<img src="../images/voorstellen/john.jpg" alt="" width="128" height="193" title="John van Es"/> 
+                	<img src="images/voorstellen/john.jpg" alt="" width="128" height="193" title="John van Es"/> 
                 	<figcaption>John van Es</figcaption>
                	</div>
             	<div class="commissie" >
-            		<img src="../images/voorstellen/monique.jpg" alt="" width="128" height="193" title="Monique <br/> Aspers-Counotte"/>
+            		<img src="images/voorstellen/monique.jpg" alt="" width="128" height="193" title="Monique <br/> Aspers-Counotte"/>
             		<figcaption>Monique Aspers-Counotte</figcaption>
               	</div>                    
                 </figure>
