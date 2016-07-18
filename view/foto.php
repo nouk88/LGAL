bcf2147543a1912214e0ddccad7911047958cce9
aclassens <aclassens@REA5116.reacollege.eu>
2016-07-18 10:16:39 +0200

    fotopagina afgemaakt

--- view/foto.php
+++ view/foto.php
@@ -4,14 +4,18 @@ function showFoto()
 {
     
     echo '
-    <h2><h2>    
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <h2>Foto\'s</h2>
+    <h3>Boeren golf<h3>    
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
         <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
             <li data-target="#myCarousel" data-slide-to="2"></li>
             <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+            <li data-target="#myCarousel" data-slide-to="6"></li>
         </ol>
 
         <!-- Wrapper for slides -->
@@ -30,6 +34,139 @@ function showFoto()
 
             <div class="item">
                 <img src="images/boeren_golf/golf5.jpg" alt="golf5">
+            </div>
+            
+            <div class="item">
+                <img src="images/boeren_golf/golf6.jpg" alt="golf6">
+            </div>
+
+            <div class="item">
+                <img src="images/boeren_golf/golf7.jpg" alt="golf7">
+            </div>
+
+            <div class="item">
+                <img src="images/boeren_golf/golf8.jpg" alt="golf8">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    
+    <h3>Blote voetenpad, Brunssum<h3>    
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+            <li data-target="#myCarousel" data-slide-to="6"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="images/blote_voetenpad/beetje_vies_geworden.jpg" alt="beetje vies geworden">
+            </div>
+
+            <div class="item">
+                <img src="images/blote_voetenpad/blote_voetenpad1.jpg" alt="lopen door steentjes">
+            </div>
+
+            <div class="item">
+                <img src="images/blote_voetenpad/groep_halverwege.jpg" alt="groepsfoto op de helft van de route">
+            </div>
+
+            <div class="item">
+                <img src="images/blote_voetenpad/leo_water_oversteek.jpg" alt="Leo steekt het water over">
+            </div>
+            
+            <div class="item">
+                <img src="images/blote_voetenpad/manno.jpg" alt="Manno gehurkt">
+            </div>
+
+            <div class="item">
+                <img src="images/blote_voetenpad/wassen.jpg" alt="de modder afwassen na de wandeling">
+            </div>
+
+            <div class="item">
+                <img src="images/blote_voetenpad/koffie.jpg" alt="Lekker iets drinken">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <h3>Overige foto\'s<h3>    
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+            <li data-target="#myCarousel" data-slide-to="6"></li>
+            <li data-target="#myCarousel" data-slide-to="7"></li>
+            <li data-target="#myCarousel" data-slide-to="8"></li>
+            <li data-target="#myCarousel" data-slide-to="9"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="images/overig/bbq1.jpg" alt="schuilen onder de partytent">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/bbq2.jpg" alt="lekker vlees op de bbq">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/kerststukje2.JPG" alt="kerststukjes maken">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/kerststukje3.JPG" alt="kerststukjes maken">
+            </div>
+            
+            <div class="item">
+                <img src="images/overig/kerststukje4.JPG" alt="kerststukjes maken">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/mini_golf1.jpg" alt="mini golf">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/mini_golf2.jpg" alt="mini golf">
+            </div>
+            
+            <div class="item">
+                <img src="images/overig/mini_golf3.jpg" alt="mini golf">
+            </div>
+
+            <div class="item">
+                <img src="images/overig/wokken_30_3_2015.jpg" alt="Lekker wokken">
             </div>
         </div>
 
